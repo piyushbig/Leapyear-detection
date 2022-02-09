6b6b147f96a0d4b5a9e2118e52a282794e5cdaf3
piyushbig <74531950+piyushbig@users.noreply.github.com>
2022-02-09 17:54:27 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,26 @@
+function isLeap(year) {
+    
+/**************Don't change the code above****************/    
+    
+    //Write your code here.    
+    
+    if( year % 4 === 0){
+        console.log("Leap year.")
+    }
+    if(year % 100===0){
+        console.log("Not leap year.")
+    }
+    if(year%400===0){
+        console.log("Leap year.")
+    }
+    else{
+        console.log("Not leap year.")
+    }
+    
+
+
+    
+
+/**************Don't change the code below****************/    
+
+}
